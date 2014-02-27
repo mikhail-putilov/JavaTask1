6ee25c1b3ce528902cadaac8a3555df9e8b08428
Putilov Michael <post.snowy@gmail.com>
2014-02-27 14:34:47 +0600

    Add try catch exception handling in sort.

--- RowsComparator.java
+++ RowsComparator.java
@@ -11,8 +11,13 @@ public class RowsComparator implements Comparator<String[]> {
 
     @Override
     public int compare(final String[] row1, final String[] row2) {
-        String cell_1 = row1[columnIndex];
-        String cell_2 = row2[columnIndex];
-        return comparator.compare(cell_1, cell_2);
+        try {
+            String cell_1 = row1[columnIndex];
+            String cell_2 = row2[columnIndex];
+            return comparator.compare(cell_1, cell_2);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("Rows can't be compared by column index: "+ columnIndex);
+            throw new IndexOutOfBoundsException(e.getMessage());
+        }
     }
 }
